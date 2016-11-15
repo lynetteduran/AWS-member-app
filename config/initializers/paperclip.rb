31f560980de678c396731020e2a17451b38acd52
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-11-14 16:36:04 -0800

    fixes paperclip.rb formatting

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -7,7 +7,8 @@ Paperclip::Attachment.default_options[:s3_protocol] = "https"
 Paperclip::Attachment.default_options[:s3_host_name] = "s3-us-west-2.amazonaws.com"
 
 Paperclip::Attachment.default_options[:default_url] = lambda { |image|
-    ActionController::Base.helpers.asset_path('sample.jpg')
+
+ActionController::Base.helpers.asset_path('sample.jpg')
 }
 
 Paperclip::Attachment.default_options[:s3_credentials] = {
